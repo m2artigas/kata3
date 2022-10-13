41a45133402d571d320895b1b79d2ed1ae2ca933
m2artigas <m2artigas@gmail.com>
2022-10-13 12:24:37 +0100

    versión 1

--- src/src/HistogramDisplay.java
+++ src/src/HistogramDisplay.java
@@ -1,8 +1,13 @@
 package src;
 
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class HistogramDisplay extends ApplicationFrame {
@@ -12,8 +17,29 @@ public class HistogramDisplay extends ApplicationFrame {
         pack();
     }
 
-    private ChartPanel createPanel() {
-        return null;
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataSet()));
+        chartPanel.setPreferredSize(new Dimension(500, 400));
+        return chartPanel;
+    }
+
+    private DefaultCategoryDataset createDataSet() {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(3, "", "ulpgc.es");
+        dataset.addValue(7, "", "dis.ulpgc.es");
+        dataset.addValue(11, "", "eii.ulpgc.es");
+        dataset.addValue(3, "", "gmail.com");
+        dataset.addValue(5, "", "hotmail.com");
+        return dataset;
+    }
+
+    private JFreeChart createChart(DefaultCategoryDataset dataset) {
+        JFreeChart chart = ChartFactory.createBarChart("Histogram Display",
+                "Dominios email",
+                "Nº emails", dataset,
+                PlotOrientation.VERTICAL, false,
+                false, false);
+        return chart;
     }
 
     public void execute(){
